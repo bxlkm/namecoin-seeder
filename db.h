b7355505083e7fe74de7cd2310a0e91e269e4288
Hugo Landau <hlandau@devever.net>
2014-12-22 23:34:48 +0000

    Relax version requirement.

--- db.h
+++ db.h
@@ -12,7 +12,9 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 40000
+// NAMECOIN
+#define REQUIRE_VERSION 35000
+//#define REQUIRE_VERSION 40000
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
